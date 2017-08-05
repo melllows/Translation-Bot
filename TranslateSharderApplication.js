48cf8ad5623510505bb6bfa73401aad52670e126
젠장 내 엉덩이 <TannerReynolds@Outlook.com>
2017-08-05 07:50:12 -0700

    Added SharderApplication

---
+++ TranslateSharderApplication.js
@@ -0,0 +1,4 @@
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager(`GTrans.js`, { totalShards: 2 });
+manager.spawn();
+manager.on('launch', shard => console.log(`Successfully launched shard ${shard.id}`));
