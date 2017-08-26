57bb3a178922b8f0ae2baf97002b505b96bdda5c
구두끈 <treynolds4@student.yosemite.edu>
2017-08-26 15:27:00 -0700

    not needed

--- TranslateSharderApplication.js
+++
@@ -1,4 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager(`GTrans.js`, { totalShards: 1 });
-manager.spawn();
-manager.on('launch', shard => console.log(`Successfully launched shard ${shard.id}`));
